c48d09596924e2116b39edf6e15660c62531c4a3
makar <makmakhorin@hotmail.com>
2025-05-15 17:22:17 +0400

    Add project files.

---
+++ Vector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector_Class
+{
+    public class Vector
+    {
+        int X;
+        int Y;
+        int Length;
+        public Vector()
+        {
+            Length = MathF.Sqrt(X * X + Y * Y);
+        }
+        public override string ToString()
+        {
+            return $"Длина вектора ({X}, {Y}): {Length}";
+        }
+    }
+}
